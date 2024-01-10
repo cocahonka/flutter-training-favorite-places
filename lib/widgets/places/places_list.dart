70fb993532a95d25803f1022888359e0b393ce2e
Ivan <cocahonka@gmail.com>
2024-01-10 13:49:50 +0300

    made up circle image preview for each place in place list

--- lib/widgets/places/places_list.dart
+++ lib/widgets/places/places_list.dart
@@ -31,18 +31,28 @@ class _PlacesListState extends State<PlacesList> {
               style: Theme.of(context).textTheme.titleSmall,
             ),
           )
-        : ListView.builder(
-            itemCount: places.length,
-            itemBuilder: (_, index) {
-              final place = places[index];
-              return ListTile(
-                title: Text(
-                  place.title,
-                  style: Theme.of(context).textTheme.titleMedium,
-                ),
-                onTap: () => _openPlaceDetailsScreen(place),
-              );
-            },
+        : Padding(
+            padding: const EdgeInsets.all(8),
+            child: ListView.separated(
+              itemCount: places.length,
+              itemBuilder: (_, index) {
+                final place = places[index];
+                return ListTile(
+                  leading: CircleAvatar(
+                    radius: 26,
+                    backgroundImage: FileImage(place.image),
+                  ),
+                  title: Text(
+                    place.title,
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
+                  onTap: () => _openPlaceDetailsScreen(place),
+                );
+              },
+              separatorBuilder: (context, index) {
+                return const Divider();
+              },
+            ),
           );
   }
 }
