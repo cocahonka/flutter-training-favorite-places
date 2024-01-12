c7eb6f053edde6e46f4c5622d4418044d6a44e16
Ivan <cocahonka@gmail.com>
2024-01-12 17:43:55 +0300

    integrate yandex mapkit in app

--- android/app/src/main/kotlin/com/example/favorite_places/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/favorite_places/MainActivity.kt
@@ -1,6 +1,13 @@
 package com.example.favorite_places
 
+import androidx.annotation.NonNull
+import com.yandex.mapkit.MapKitFactory
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        MapKitFactory.setApiKey("a5039f3d-4ead-45d7-9084-42248918d28e")
+        super.configureFlutterEngine(flutterEngine)
+    }
 }
