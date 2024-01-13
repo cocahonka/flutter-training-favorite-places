db1605427243052c8a5351036a849a4a82703373
Ivan <cocahonka@gmail.com>
2024-01-13 12:31:20 +0300

    fill up place details screen with more info

--- lib/screens/place_details_screen.dart
+++ lib/screens/place_details_screen.dart
@@ -9,6 +9,13 @@ class PlaceDetailsScreen extends StatelessWidget {
 
   final Place place;
 
+  String get _getStaticMapUrl {
+    final (latitude, longitude) = (place.location.latitude, place.location.longitude);
+    final url =
+        'https://static-maps.yandex.ru/v1?ll=$longitude,$latitude&size=600,300&z=16&pt=$longitude,$latitude,comma&apikey=868c8c70-f093-45c6-bbc2-7f4f6ccb64e5';
+    return url;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,6 +32,41 @@ class PlaceDetailsScreen extends StatelessWidget {
             fit: BoxFit.cover,
             width: double.infinity,
             height: double.infinity,
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Column(
+              children: [
+                CircleAvatar(
+                  radius: 70,
+                  backgroundImage: NetworkImage(_getStaticMapUrl),
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 24,
+                    vertical: 16,
+                  ),
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Colors.transparent,
+                        Colors.black54,
+                      ],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                    ),
+                  ),
+                  child: Text(
+                    place.location.address,
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.titleLarge,
+                  ),
+                )
+              ],
+            ),
           ),
         ],
       ),
