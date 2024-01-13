080dbe554be3fa553db7edd82d6ae9b9184dc44d
Ivan <cocahonka@gmail.com>
2024-01-13 13:27:37 +0300

    open map view from place details screen

--- lib/screens/place_details_screen.dart
+++ lib/screens/place_details_screen.dart
@@ -1,7 +1,8 @@
 import 'package:favorite_places/models/place.dart';
+import 'package:favorite_places/screens/map_screen.dart';
 import 'package:flutter/material.dart';
 
-class PlaceDetailsScreen extends StatelessWidget {
+class PlaceDetailsScreen extends StatefulWidget {
   const PlaceDetailsScreen({
     required this.place,
     super.key,
@@ -9,11 +10,24 @@ class PlaceDetailsScreen extends StatelessWidget {
 
   final Place place;
 
+  @override
+  State<PlaceDetailsScreen> createState() => _PlaceDetailsScreenState();
+}
+
+class _PlaceDetailsScreenState extends State<PlaceDetailsScreen> {
   String get _getStaticMapUrl {
-    final (latitude, longitude) = (place.location.latitude, place.location.longitude);
+    final (latitude, longitude) = (widget.place.location.latitude, widget.place.location.longitude);
     final url =
         'https://static-maps.yandex.ru/v1?ll=$longitude,$latitude&size=600,300&z=16&pt=$longitude,$latitude,comma&apikey=868c8c70-f093-45c6-bbc2-7f4f6ccb64e5';
     return url;
+  }
+
+  void _openMapScreen() {
+    Navigator.of(context).push<void>(
+      MaterialPageRoute(
+        builder: (_) => MapScreen(location: widget.place.location),
+      ),
+    );
   }
 
   @override
@@ -21,14 +35,14 @@ class PlaceDetailsScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          place.title,
+          widget.place.title,
           style: Theme.of(context).textTheme.titleLarge,
         ),
       ),
       body: Stack(
         children: [
           Image.file(
-            place.image,
+            widget.place.image,
             fit: BoxFit.cover,
             width: double.infinity,
             height: double.infinity,
@@ -39,9 +53,12 @@ class PlaceDetailsScreen extends StatelessWidget {
             right: 0,
             child: Column(
               children: [
-                CircleAvatar(
-                  radius: 70,
-                  backgroundImage: NetworkImage(_getStaticMapUrl),
+                GestureDetector(
+                  onTap: _openMapScreen,
+                  child: CircleAvatar(
+                    radius: 70,
+                    backgroundImage: NetworkImage(_getStaticMapUrl),
+                  ),
                 ),
                 Container(
                   alignment: Alignment.center,
@@ -60,7 +77,7 @@ class PlaceDetailsScreen extends StatelessWidget {
                     ),
                   ),
                   child: Text(
-                    place.location.address,
+                    widget.place.location.address,
                     textAlign: TextAlign.center,
                     style: Theme.of(context).textTheme.titleLarge,
                   ),
